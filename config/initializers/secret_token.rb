782ebafe5b8c0183277dd3795b248eff3aa614e4
Ahmad hamza <ahmadhamza19@gmail.com>
2013-09-24 01:11:30 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BookRental::Application.config.secret_token = 'bfcd4eca727feec7c84d7b2597461b8ba6fdd07231a691431f32d3e0e13e7c32c5c494906741222d64384161fd6e19603274407e7ab4901c16dde1f5a03288b5'
